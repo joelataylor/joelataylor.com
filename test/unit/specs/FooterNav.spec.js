2550fe17e93992e3a1d4596c7353ee50a32005b7
Joel Taylor <joelataylor@gmail.com>
2017-12-10 13:32:11 -0500

    Sweet, tests with Vuex.state! files please

---
+++ test/unit/specs/FooterNav.spec.js
@@ -0,0 +1,22 @@
+import { shallow, createLocalVue } from 'vue-test-utils'
+import Vuex from 'vuex'
+import originalState from '@/store'
+import FooterNav from '@/components/FooterNav'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('FooterNav', () => {
+  let store
+
+  beforeEach(() => {
+    store = originalState
+  })
+
+  it('should render my name', () => {
+    const wrapper = shallow(FooterNav, { store, localVue })
+    const el = wrapper.find('#copyright')
+    //console.log(el.text())
+    expect(el.text()).toEqual("😀   © 2018   Joel Taylor")
+  })
+})
